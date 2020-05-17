04e74b974b9a83df40688cd712667f46cdf6b23d
Isaac Lee <isaacleewebdev@gmail.com>
2020-05-16 17:01:31 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://cdn.shopify.com/s/files/1/0070/7235/0326/products/Doggodog_Corgi_Head_StickerLayout_01-01_2048x.jpg?v=1536140349" className="App-logo" alt="logo" />
+        <h1
+          style={{marginTop: "50px"}}>
+          Happy birthday Nico!</h1>
+          <p>Hope you had a good one, dawg.</p>
       </header>
     </div>
   );
