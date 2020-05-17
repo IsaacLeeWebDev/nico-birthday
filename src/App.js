179f902608ab08995f1e34e1c9f119e831a03bb9
Isaac Lee <isaacleewebdev@gmail.com>
2020-05-16 18:35:30 -0700

    add style

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <h1 style={{marginTop: "50px"}}>Happy birthday Nico!</h1>
         <h3 style={{marginBottom: "600px"}}>(scroll down)</h3>
         <img
-        style={{margin: "50px"}}
+          style={{margin: "300px"}}
           src="https://cdn.shopify.com/s/files/1/0070/7235/0326/products/Doggodog_Corgi_Head_StickerLayout_01-01_2048x.jpg?v=1536140349"
           className="App-logo"
           alt="logo"
