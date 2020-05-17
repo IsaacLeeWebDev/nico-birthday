963384bd4698c20402f6a2694487abe5145d5350
Isaac Lee <isaacleewebdev@gmail.com>
2020-05-16 17:29:42 -0700

    attempt two

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <audio autoPlay>
-        <source src="./song.mp3" type="audio/mpeg" />
-      </audio>
       <header className="App-header">
         <img
           src="https://cdn.shopify.com/s/files/1/0070/7235/0326/products/Doggodog_Corgi_Head_StickerLayout_01-01_2048x.jpg?v=1536140349"
@@ -17,6 +14,9 @@ function App() {
         <h1 style={{marginTop: "50px"}}>Happy birthday Nico!</h1>
         <p>Hope you had a good one, dawg.</p>
       </header>
+      <audio controls autoPlay>
+        <source src="./song.mp3" type="audio/mpeg" />
+      </audio>
     </div>
   );
 }
