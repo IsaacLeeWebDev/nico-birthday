82dbab8f118d38957e36faa451bbf5522443c614
Isaac Lee <isaacleewebdev@gmail.com>
2020-05-16 18:32:40 -0700

    make it a 2-parter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -6,17 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 style={{marginTop: "50px"}}>Happy birthday Nico!</h1>
+        <h3 style={{marginBottom: "600px"}}>(scroll down)</h3>
         <img
+        style={{margin: "50px"}}
           src="https://cdn.shopify.com/s/files/1/0070/7235/0326/products/Doggodog_Corgi_Head_StickerLayout_01-01_2048x.jpg?v=1536140349"
           className="App-logo"
           alt="logo"
         />
-        <h1 style={{marginTop: "50px"}}>Happy birthday Nico!</h1>
-        <p>Hope you had a good one, dawg.</p>
+        <h1>Hope it's a delight, dawg.</h1>
       </header>
-      <audio controls autoPlay>
-        <source src="./song.mp3" type="audio/mpeg" />
-      </audio>
     </div>
   );
 }
